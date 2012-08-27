161dc883d476fabf8ba21818c5fde123c5357b41
luoyulong <luoceanskold@qq.com>
2012-08-27 16:28:45 +0800

    创建了过滤文件 .gitignore
    不需要同步的文件可以在.gitignore中用规则过滤

--- properties.h
+++ properties.h
@@ -36,158 +36,176 @@
 #define int TRANSP_MATRIX  1
 #define int HERMIT_MATRIX  2
 
-int get_descra(IN char[] descra, IN int descriptor, OUT char *message, OUT int *ierr)
+      contains      
+
+      subroutine get_descra(descra,descriptor,message,ierr)
+      implicit none
+      character*11, intent(in) :: descra
+      character, intent(in) :: descriptor
+      character, intent(out) :: message
+      integer, intent(out) :: ierr
+
+void get_descra(char *descra,char descriptor, char &message,int &ierr)
 {
-    *ierr = -1;
-    message ='';
-    switch (descriptor)
-    {
-        case ('a')
-            message=descra[2];
+    
+    ierr = -1;
+    message = '';
+    switch (descriptor) {
+        case('a') //lower,upper or both parts
+            message = descra(3:3);break;
+        case('b') //base
+            message = descra(5:5);break;
+        case('d') //unity diagonal stored or not
+            message = descra(1:1);break;
+        case('f') //internal block storage is row- or column-wise
+            message = descra(7:7);break;
+        case('r') //repeated indices
+            message = descra(2:2);break;
+        case('s') //structure of matrix
+            message = descra(4:4);break;
+        case('t') //matrix type
+            message = descra(6:6);break;
+        default:return;
             break;
-        case ('b')
-            message=descra[4];
-            break;
-        case ('d')
-            message=descra[0];
-            break;
-        case ('f')
-            message=descra[6];
-            break;
-        case ('r')
-            message=descra[1];
-            break;
-        case('s')
-            message=descra[3];
-            break;
-        case('t')
-            message=descra[5];
-            break;
-        case default:
-            return -1;
-            *ierr = 0;
-            return 0;
+            ierr=0;
     }
+    
 }
 
-void set_descra(OUT char[] descra, IN int prpty, OUT int *istat)
+
+
+      subroutine set_descra(descra,prpty,ierr)
+      character*11, intent(out) :: descra
+      integer, intent(in) :: prpty
+      integer, intent(out) :: ierr
+
+
+void set_descra(char *descra,int prpty,int &ierr)
 {
     int dummy;
-    decra = '';
-    *ierr = -1;
-    dummpy = prpty;
-    //check if matrix has unstroed unity diagomal
-    if (dummy % 2 == 1)
-        decra[0]='U';
-    else
-        decra[0]='N';
-    dummy = dummy - dummy%2;
-    // repeated indices
-    if (dummy%4 == 2)
-        decra[1]='R';
-    else
-        decra[1]='U' //default
-    dummy = dummy - dummy % 4;
-    //both/lower/upper half of matrix specified
-    switch (dummy % 16)
-    {
-        case (0):
-            decra[2] = 'B' //default
-            break;
-        case (4):
-            decra[2] = 'U' 
-            break;
-        case (8):
-            decra[2] = 'L' 
-            break;
-        case default:
-            return -1;
-        dummy = dummy - dummy %16;
-    //matrix is irregular/reguarl/unassebmled
-    switch (dummy % 64)
-    {
-        case (0):
-            decra[3] = 'I' //default
-            break;
-        case (16):
-            decra[3] = 'R' 
-            break;
-        case (32):
-            decra[3] = 'U' 
-            break;
-        case default:
-            return -1;
-    }
-    dummy = dummy - dummy % 64;
-    //index base
-    if (dummy % 128  ==64)
-        decra[4]='C';
-    eles
-        decra[4]='F';
-    }
-    dummy = dummy - dummy % 128;
-    //matrix type
-    switch (dummy % 1024)
-    {
-        case 0:
-            decra[5] = 'G';
-            break;
-        case 128:
-            decra[5] = 'S' //default
-            break;
-        case 256:
-            decra[5] = 'H' 
-            break;
-        case (512):
-            decra[5] = 'T' 
-            break;
-        case default:
-            return -1;
-    }
-    dummy = dummy - dummy % 1024;
-    //index base
-    if (dummy % 2048 == 1024)
-        decra[6]='R';
-    eles
-        decra[6]='C';
-    }
-   *ierr = 0; 
+    descra = ''
+    ierr = -1
+    dummy = prpty;
+      //check, if matrix has unstored unity diagonal
+      if (dummy%2==1) then
+         descra(1:1) = 'U';
+      else
+          descra(1:1) = 'N';//DEFAULT
+    dummy = dummy -dummy%2;
+      //repeated indices 
+      if (dummy%4==2) then
+         descra(2:2) = 'R';
+      else
+         descra(2:2) = 'U';//DEFAULT
+    dummy = dummy - dummy%4;      
+      //both/lower/upper half of matrix specified
+      select case(mod(dummy,16))
+      case(0)
+         descra(3:3) = 'B' //DEFAULT
+      case(4)
+         descra(3:3) = 'U'
+      case(8)
+         descra(3:3) = 'L'
+      case default
+         return
+      end select
+      dummy = dummy - mod(dummy,16)      
+      //matrix is irregular/regular/unassembled
+      select case(mod(dummy,64))
+      case(0)
+         descra(4:4) = 'I' //DEFAULT
+      case(16)
+         descra(4:4) = 'R'
+      case(32)
+         descra(4:4) = 'U'
+      case default
+         return
+      end select
+      dummy = dummy - mod(dummy,64)      
+      //index base
+      if (mod(dummy,128).eq.64) then
+         descra(5:5) = 'C' 
+      else 
+         descra(5:5) = 'F' //DEFAULT
+      end if
+      dummy = dummy - mod(dummy,128)      
+      // matrix type
+      select case(mod(dummy,1024))
+      case (0)
+         descra(6:6) = 'G' //DEFAULT
+      case(128)
+         descra(6:6) = 'S'
+      case(256)
+         descra(6:6) = 'H'
+      case(512)
+         descra(6:6) = 'T'
+      case default
+         return
+      end select
+      dummy = dummy - mod(dummy,1024)      
+      //internal block storage
+      if (mod(dummy,2048).eq.1024) then
+         descra(7:7) = 'R' 
+      else 
+         descra(7:7) = 'C' //DEFAULT
+      end if
+      dummy = dummy - mod(dummy,2048)      
+      ierr = 0
 }
+      subroutine get_infoa(infoa,descr,val,ierr)
+      implicit none
+      integer, dimension(10), intent(in) :: infoa
+      character, intent(in) :: descr
+      integer, intent(out) :: val,ierr
+      val = -1
+      ierr = -1
+      select case(descr)
+      case('b') //base of array indices
+         val = infoa(2)
+      case('c') //copy or not
+         val = infoa(9)
+      case('d') //multidim array:row-dim of block
+         val = infoa(3)
+      case('e') //multidim array:col-dim of block
+         val = infoa(4)
+      case('f') //Block structure array:row-dim in blocks
+         val = infoa(5)
+      case('g') //Block structure array:col-dim in blocks
+         val = infoa(6)
+      case('n') //nnz
+         val = infoa(1)
+      case default
+         return
+      end select
+      ierr = 0
+      end subroutine get_infoa
 
-int get_infoa(IN int[10] infoa, IN int descr, OUT int *val, OUT int *ierr)
-{
-    *val =  -1;
-    *ierr = -1;
-    switch (descr)
-    {
-        case 'b': //base of array indices
-            *val = infoa[1];
-            break;
-        case 'c':  //copy or not
-            *val = infoa[8];
-            break;
-        case 'd':  //multidim array:row-dim of block 
-            *val = infoa[2];
-            break;
-        case 'e': //multidim arrau: col-dim of block 
-            *val = infoa[3];
-            break;
-        case 'f': //block structure array: row-dim inblocks
-            *val = infoa[4];
-            break;
-        case 'g': //block structure array: col-dim inblocks
-            *val = infoa[5];
-            break;
-        case 'n': 
-            *val = infoa[0];
-            break;
-        case default:
-            return -1;
-    }
-    return 0;
-}
-//todolater
-int set_infoa(INOUT char[10] infoa, IN char descr,IN int val,OUT int *ierr)
-{
-}
-
+      subroutine set_infoa(infoa,descr,val,ierr)
+      implicit none
+      integer, dimension(10), intent(inout) :: infoa
+      character*1, intent(in) :: descr
+      integer, intent(in) :: val
+      integer, intent(out) :: ierr
+      ierr = -1
+      if (val.lt.0) return 
+      select case(descr)
+      case('b') //base of array indices
+         infoa(2) = val
+      case('c') //copy or not
+         infoa(9) = val
+      case('d') //multidim array:row-dim of blocks
+         infoa(3) = val
+      case('e') //multidim array:col-dim of blocks
+         infoa(4) = val
+      case('f') //Block structure array:row-dim in blocks
+         infoa(5) = val
+      case('g') //Block structure array:col-dim in blocks
+         infoa(6) = val
+      case('n') //nnz
+         infoa(1) = val
+      case default
+         return
+      end select
+      ierr = 0
+      end subroutine set_infoa
+      end module properties
