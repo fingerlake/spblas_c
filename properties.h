b280875421e94b9a93f0075e93a04b7d78b6a2d5
luoyulong <luoceanskold@qq.com>
2012-08-29 16:59:11 +0800

    完成properties的修改: properties.h
    主要涉及函数:set_infoa(),get_infoa(),set_descra(),get_descra()

--- properties.h
+++ properties.h
@@ -18,176 +18,207 @@
 #include "blas_sparse_namedconstants.h"
 
 // *** Description of basic derived data types
-#define int no_of_types  5
-#define int ISP_MATRIX  0
-#define int DSP_MATRIX  1
-#define int SSP_MATRIX  2
-#define int CSP_MATRIX  3
-#define int ZSP_MATRIX  4
+#define   no_of_types  5
+#define   ISP_MATRIX  0
+#define   DSP_MATRIX  1
+#define   SSP_MATRIX  2
+#define   CSP_MATRIX  3
+#define   ZSP_MATRIX  4
 
 // *** Determine, if array indices start at 0 or 1
-#define int C_BASE  0
-#define int F_BASE  1
+#define   C_BASE  0
+#define   F_BASE  1
 // *** Determine, if matrix is reference or copy of original data
-#define int REF_OF_SOURCE  0
-#define int COP_OF_SOURCE  1
+#define   REF_OF_SOURCE  0
+#define   COP_OF_SOURCE  1
 // *** Determine, if the matrix is needed or its (conjugate) transpose
-#define int ORIGIN_MATRIX  0
-#define int TRANSP_MATRIX  1
-#define int HERMIT_MATRIX  2
+#define  ORIGIN_MATRIX  0
+#define  TRANSP_MATRIX  1
+#define  HERMIT_MATRIX  2
 
-int get_descra(IN char[] descra, IN int descriptor, OUT char *message, OUT int *ierr)
+
+
+
+void get_descra(char* descra,int descriptor,char &message,int &ierr)
 {
-    *ierr = -1;
-    message ='';
-    switch (descriptor)
+  ierr = -1;
+  message ='';
+  switch (descriptor)
     {
-        case ('a')
-            message=descra[2];
-            break;
-        case ('b')
-            message=descra[4];
-            break;
-        case ('d')
-            message=descra[0];
-            break;
-        case ('f')
-            message=descra[6];
-            break;
-        case ('r')
-            message=descra[1];
-            break;
-        case('s')
-            message=descra[3];
-            break;
-        case('t')
-            message=descra[5];
-            break;
-        case default:
-            return -1;
-            *ierr = 0;
-            return 0;
+    case 'a':
+      message=descra[2];
+      break;
+    case 'b':
+      message=descra[4];
+      break;
+    case 'd':
+      message=descra[0];
+      break;
+    case 'f':
+      message=descra[6];
+      break;
+    case 'r':
+      message=descra[1];
+      break;
+    case's':
+      message=descra[3];
+      break;
+    case't':
+      message=descra[5];
+      break;
+    default:
+      return;
     }
+  ierr = 0;
 }
 
-void set_descra(OUT char[] descra, IN int prpty, OUT int *istat)
+void set_descra(char* descra, int prpty,int ierr)
 {
-    int dummy;
-    decra = '';
-    *ierr = -1;
-    dummpy = prpty;
-    //check if matrix has unstroed unity diagomal
-    if (dummy % 2 == 1)
-        decra[0]='U';
-    else
-        decra[0]='N';
-    dummy = dummy - dummy%2;
-    // repeated indices
-    if (dummy%4 == 2)
-        decra[1]='R';
-    else
-        decra[1]='U' //default
-    dummy = dummy - dummy % 4;
-    //both/lower/upper half of matrix specified
-    switch (dummy % 16)
+  int dummy;
+  descra='';
+  ierr = -1;
+  dummpy = prpty;
+  //check if matrix has unstroed unity diagomal
+  if (dummy%2== 1)
+    descra[0]='U';
+  else
+    descra[0]='N';
+  dummy=dummy - dummy%2;
+  // repeated indices
+  if (dummy%4 == 2)
+    descra[1]='R';
+  else
+    descra[1]='U'; //default
+  dummy = dummy - dummy % 4;
+  //both/lower/upper half of matrix specified
+  switch (dummy % 16)
     {
-        case (0):
-            decra[2] = 'B' //default
-            break;
-        case (4):
-            decra[2] = 'U' 
-            break;
-        case (8):
-            decra[2] = 'L' 
-            break;
-        case default:
-            return -1;
-        dummy = dummy - dummy %16;
-    //matrix is irregular/reguarl/unassebmled
-    switch (dummy % 64)
-    {
-        case (0):
-            decra[3] = 'I' //default
-            break;
-        case (16):
-            decra[3] = 'R' 
-            break;
-        case (32):
-            decra[3] = 'U' 
-            break;
-        case default:
-            return -1;
-    }
-    dummy = dummy - dummy % 64;
-    //index base
-    if (dummy % 128  ==64)
-        decra[4]='C';
-    eles
-        decra[4]='F';
-    }
-    dummy = dummy - dummy % 128;
-    //matrix type
-    switch (dummy % 1024)
-    {
+    case  0:
+      descra[2] = 'B'; //default
+      break;
+    case 4:
+      descra[2] = 'U' ;
+      break;
+    case 8:
+      descra[2] = 'L' ;
+      break;
+    default:
+      return ;
+      dummy = dummy - dummy %16;
+      //matrix is irregular/reguarl/unassebmled
+      switch (dummy % 64)
+        {
         case 0:
-            decra[5] = 'G';
-            break;
+          descra[3] = 'I'; //default
+          break;
+        case 16:
+          descra[3] = 'R';
+          break;
+        case 32:
+          descra[3] = 'U';
+          break;
+        default:
+          return ;
+        }
+      dummy = dummy - dummy % 64;
+      //index base
+      if (dummy % 128  ==64)
+        descra[4]='C';
+      eles
+          descra[4]='F';
+
+      dummy = dummy - dummy % 128;
+      //matrix type
+      switch (dummy % 1024)
+        {
+        case 0:
+          descra[5] = 'G';
+          break;
         case 128:
-            decra[5] = 'S' //default
-            break;
+          descra[5] = 'S'; //default
+          break;
         case 256:
-            decra[5] = 'H' 
-            break;
+          descra[5] = 'H';
+          break;
         case (512):
-            decra[5] = 'T' 
-            break;
-        case default:
-            return -1;
+          descra[5] = 'T';
+          break;
+        default:
+          return;
+        }
+      dummy = dummy - dummy % 1024;
+      //index base
+      if (dummy % 2048 == 1024)
+        descra[6]='R';
+      eles
+          descra[6]='C';
     }
-    dummy = dummy - dummy % 1024;
-    //index base
-    if (dummy % 2048 == 1024)
-        decra[6]='R';
-    eles
-        decra[6]='C';
-    }
-   *ierr = 0; 
+  ierr = 0;
 }
 
-int get_infoa(IN int[10] infoa, IN int descr, OUT int *val, OUT int *ierr)
+
+void get_infoa(int* infoa,char descr,  int &val,  int &ierr)
 {
-    *val =  -1;
-    *ierr = -1;
-    switch (descr)
+  val =  -1;
+  ierr = -1;
+  switch (descr)
     {
-        case 'b': //base of array indices
-            *val = infoa[1];
-            break;
-        case 'c':  //copy or not
-            *val = infoa[8];
-            break;
-        case 'd':  //multidim array:row-dim of block 
-            *val = infoa[2];
-            break;
-        case 'e': //multidim arrau: col-dim of block 
-            *val = infoa[3];
-            break;
-        case 'f': //block structure array: row-dim inblocks
-            *val = infoa[4];
-            break;
-        case 'g': //block structure array: col-dim inblocks
-            *val = infoa[5];
-            break;
-        case 'n': 
-            *val = infoa[0];
-            break;
-        case default:
-            return -1;
+    case 'b': //base of array indices
+      val = infoa[1];
+      break;
+    case 'c':  //copy or not
+      val = infoa[8];
+      break;
+    case 'd':  //multidim array:row-dim of block
+      val = infoa[2];
+      break;
+    case 'e': //multidim arrau: col-dim of block
+      val = infoa[3];
+      break;
+    case 'f': //block structure array: row-dim inblocks
+      val = infoa[4];
+      break;
+    case 'g': //block structure array: col-dim inblocks
+      val = infoa[5];
+      break;
+    case 'n':
+      val = infoa[0];
+      break;
+    default:
+      return ;
     }
-    return 0;
-}
-//todolater
-int set_infoa(INOUT char[10] infoa, IN char descr,IN int val,OUT int *ierr)
-{
+  return ;
 }
 
+void set_infoa(int* infoa, char descr,int val,int &ierr)
+{
+  ierr=-1;
+  if(val<0) return;
+  switch (descr)
+    {
+    case 'b': //base of array indices
+      infoa[1] = val;
+      break;
+    case('c'): //copy or not
+      infoa[8] = val;
+      break;
+    case('d'): //multidim array:row-dim of blocks
+      infoa[2] = val;
+      break;
+    case('e'): //multidim array:col-dim of blocks
+      infoa[3]= val;
+      break;
+    case('f'): //Block structure array:row-dim in blocks
+      infoa[4] = val;
+      break;
+    case('g'): //Block structure array:col-dim in blocks
+      infoa[5] = val;
+      break;
+    case('n'): //nnz
+      infoa[0] = val;
+      break;
+    default:
+      return;
+    }
+  ierr=0;
+}
